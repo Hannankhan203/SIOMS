e0079850b2ead07bffe5e8ca8c335a268607ab2b
Hannankhan203 <ak1818375@gmail.com>
2025-12-10 16:51:14 +0500

    Added Supplier page

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -8,27 +8,26 @@ namespace SIOMS.Models
         [Key]
         public int SupplierId { get; set; }
 
-        [Required]
-[StringLength(100)]
-public string Name { get; set; } = string.Empty;
-        
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
         [Required]
         [StringLength(100)]
         public string CompanyName { get; set; } = string.Empty;
-        
+
         [StringLength(100)]
         public string ContactPerson { get; set; } = string.Empty;
-        
+
         [StringLength(20)]
         public string? Phone { get; set; }
-        
+
         [EmailAddress]
         [StringLength(100)]
         public string? Email { get; set; }
-        
+
         [StringLength(200)]
         public string? Address { get; set; }
-        
+
         // Navigation property
         public virtual ICollection<Product> Products { get; set; } = new List<Product>();
         public virtual ICollection<PurchaseOrder> PurchaseOrders { get; set; } = new List<PurchaseOrder>();
--- obj/Debug/net8.0/SIOMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/SIOMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIOMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee583fa82553e5f2c94fd5a721f5048c510c1638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a19a43a700e6c46312e6813f8813f0357a4360f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIOMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIOMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
